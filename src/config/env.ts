6b10b5e3a6e000f0693eb3f0b85979bebbe2862e
SeungWon <20sunrin041@sunrint.hs.kr>
2021-04-13 11:19:36 +0900

    chore: Add monogdb container

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 const config = {
-    DB_HOST: process.env.DB_HOST || "mongodb://localhost:27017/test",
+    DB_HOST: process.env.DB_HOST || "mongodb://mongodb:27017/test",
     PORT: process.env.PORT || 3000
 }
 
