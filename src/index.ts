4e45fc9bb8a1c8a6d2f050636273fae89fb696bc
SeungWon <20sunrin041@sunrint.hs.kr>
2021-05-11 09:24:40 +0900

    feat: Add Docs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 dotenv.config()
 import env from "config/env"
 
+import { express as voyagerMiddleware } from "graphql-voyager/middleware"
 import { ApolloServer, ApolloError } from "apollo-server-express"
 import { readFileSync } from "fs"
 import { createServer } from "http"
@@ -17,8 +18,9 @@ const typeDefs = readFileSync("src/typeDefs.graphql", "utf-8")
 
 const app = express()
 app.use(bodyParserGraphQL())
-app.get("/graphql", expressPlayground({ endpoint: "/api" }))
-
+app.use("/voyager", voyagerMiddleware({ endpointUrl: "/api" }))
+app.use("/graphql", expressPlayground({ endpoint: "/api" }))
+app.use("/api-docs", express.static("docs"))
 const start = async () => {
     const db = await DB.get()
     const server = new ApolloServer({
@@ -52,7 +54,8 @@ const start = async () => {
     const httpServer = createServer(app)
     httpServer.timeout = 5000
     httpServer.listen({ port: env.PORT || 3000 }, () => {
-        console.log(`GraphQL Server Running at http://localhost:${env.PORT || 3000}/api`)
+        console.log(`GraphQL API Running at http://localhost:${env.PORT || 3000}/api`)
+        console.log(`GraphQL Docs Running at http://localhost:${env.PORT || 3000}/api-docs`)
     })
 }
 
