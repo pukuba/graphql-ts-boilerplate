7abd082f5e78212f6a9f4beaea7ddb6ecfbc3ea3
SeungWon <20sunrin041@sunrint.hs.kr>
2022-02-19 19:59:21 +0900

    test: Change assert to chai

--- src/resolvers/app/health/test.ts
+++ src/resolvers/app/health/test.ts
@@ -1,4 +1,4 @@
-import { deepStrictEqual as deepEqual } from "assert"
+import { expect } from "chai"
 import request from "supertest"
 import appPromise from "app"
 import { Server } from "http"
@@ -18,37 +18,39 @@ describe("Server running test", () => {
 	describe("Query healthLive", () => {
 		it("Should be return http status code 200", async () => {
 			const query = "{healthLive}"
-			await request(app).get(`/api?query=${query}`).expect(200)
+			const { body } = await request(app).get(`/api?query=${query}`).expect(200)
+			expect(body.data.healthLive).to.be.a("string")
 		})
 		it("Should be return http status code 400", async () => {
 			const query = "query{healthLive1}"
 			const { body } = await request(app).get(`/api?query=${query}`).expect(400)
-			deepEqual(
-				body.errors[0].message,
+			expect(body.errors[0].message).to.be.equal(
 				'Cannot query field "healthLive1" on type "Query". Did you mean "healthLive"?'
 			)
 		})
 	})
 
 	describe("Mutation testMutation", () => {
+		const query = `
+			mutation ($input: HealthCheckInput!){
+				healthCheck(input: $input)
+			}
+		`
 		it("Should be return http status code 200", async () => {
-			const query = 'mutation{healthCheck(input:{data:"ping"})}'
 			await request(app)
 				.post("/api")
 				.set("Content-Type", "application/json")
-				.send(JSON.stringify({ query }))
+				.send({ query, variables: { input: { data: "ping" } } })
 				.expect(200, { data: { healthCheck: "pong" } })
 		})
 		it("Should be return http status code 400", async () => {
-			const query = 'mutation{healthCheck(input:{data:"pong"})}'
 			const { body } = await request(app)
 				.post("/api")
 				.set("Content-Type", "application/json")
-				.send(JSON.stringify({ query }))
+				.send({ query, variables: { input: { data: "pong" } } })
 				.expect(400)
-			deepEqual(
-				body.errors[0].message,
-				'Expected type data_String_NotNull_pattern_ping!, found "pong"; Must match ^ping$'
+			expect(body.errors[0].message).to.be.equal(
+				'Variable "$input" got invalid value "pong" at "input.data"; Expected type data_String_NotNull_pattern_ping. Must match ^ping$'
 			)
 		})
 	})
