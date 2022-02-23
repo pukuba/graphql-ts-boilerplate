2d6e0b3da27818abc4b5271d2aef96c7a68d4b37
SeungWon <20sunrin041@sunrint.hs.kr>
2022-02-24 06:59:40 +0900

    feat: Add login mutation

--- src/resolvers/app/auth/Mutation.ts
+++ src/resolvers/app/auth/Mutation.ts
@@ -1,5 +1,5 @@
-import { Context, MutationRegisterArgs } from "config"
-import { createHashedPassword } from "shared"
+import { Context, MutationRegisterArgs, MutationLoginArgs } from "config"
+import { createHashedPassword, checkPassword, jwt } from "shared"
 
 export const register = async (parent: void, args: MutationRegisterArgs, ctx: Context) => {
 	const { email, password } = args.input
@@ -24,5 +24,35 @@ export const register = async (parent: void, args: MutationRegisterArgs, ctx: Co
 	return {
 		__typename: "User",
 		...user,
+	}
+}
+
+export const login = async (parent: void, args: MutationLoginArgs, ctx: Context) => {
+	const { email, password } = args.input
+	const user = await ctx.db.collection("user").findOne({ email })
+	const LoginError = { __typename: "InvalidAccountError", path: "login" }
+	if (!user) {
+		return {
+			...LoginError,
+			message: "해당 이메일에 존재하는 계정이 없습니다",
+			suggestion: "이메일을 정확하게 입력해주세요",
+		}
+	}
+	if (checkPassword(password, user.password) === false) {
+		return {
+			...LoginError,
+			message: "해당 이메일에 존재하는 계정과 입력하신 비밀번호가 일치하지 않습니다",
+			suggestion: "비밀번호를 정확하게 입력해주세요",
+		}
+	}
+	const document = await ctx.db
+		.collection("user")
+		.findOneAndUpdate({ email }, { $set: { updatedAt: Date.now() } }, { returnDocument: "after" })
+
+	const { updatedAt, createdAt } = document.value
+	return {
+		__typename: "LoginInfo",
+		user: document.value,
+		token: jwt.encode({ email, updatedAt, createdAt }, "user"),
 	}
 }
